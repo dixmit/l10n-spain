e3ec508c40a37d59f8a8a7433ae03ea4c39fb69e
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-10-09 15:18:22 +0200

    [FIX] l10n_es_facturae: Updated template name

--- l10n_es_facturae/models/account_chart_template.py
+++ l10n_es_facturae/models/account_chart_template.py
@@ -6,6 +6,10 @@ from odoo.addons.account.models.chart_template import template
 class AccountChartTemplate(models.AbstractModel):
     _inherit = "account.chart.template"
 
-    @template("es_common", "account.tax")
+    @template("es_common_mainland", "account.tax")
+    def _get_es_facturae_account_tax(self):
+        return self._parse_csv("es_common", "account.tax", module="l10n_es_facturae")
+
+    @template("es_canary_common", "account.tax")
     def _get_es_facturae_account_tax(self):
         return self._parse_csv("es_common", "account.tax", module="l10n_es_facturae")
